14a7ba86d387de05ce281b747e1b040e68abd950
Nick James <thenickjames@gmail.com>
2019-07-10 17:09:32 +0700

    new login page

--- react2/src/App.js
+++ react2/src/App.js
@@ -3,9 +3,12 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import { Router, Link } from "@reach/router";
 
+import CssBaseline from "@material-ui/core/CssBaseline"
+
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Router>
         <Register path="register"/>
         <Login path="login"/>
--- react2/src/pages/Login.js
+++ react2/src/pages/Login.js
@@ -1,42 +1,120 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 import { useFormFields } from "../hooks/forms";
 import { login } from "../api/auth";
 
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
 function Login(props) {
+  const classes = useStyles();
   const { fields, handleChange } = useFormFields({
-      username: "",
-      password: ""
+    email: "",
+    password: ""
   });
 
-  const submit = (event) => {
+  const submit = event => {
     event.preventDefault();
     const result = login(fields.username, fields.password);
-  }
+  };
 
   return (
-    <>
-      <h1>Log in</h1>
-      <form onSubmit={submit}>
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          value={fields.username}
-          onChange={handleChange}
-          required
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={fields.password}
-          onChange={handleChange}
-          required
-        />
-        <input type="submit" />
-      </form>
-    </>
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={submit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={fields.email}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={fields.password}
+            onChange={handleChange}
+          />
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
 
