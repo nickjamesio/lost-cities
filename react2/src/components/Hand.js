f18f113502e726ade01d194a6058271c722065a2
Nick James <thenickjames@gmail.com>
2019-07-22 14:32:42 +0700

    use position for hand

--- react2/src/components/Hand.js
+++ react2/src/components/Hand.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useGameState } from "../context/GameContext";
 import { makeStyles } from "@material-ui/core";
 
 import Card from "../components/Card";
@@ -11,32 +10,64 @@ const useStyles = makeStyles(theme => ({
   },
   row: {
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
+    position: "relative",
+    height: "200px",
+    "&:nth-child(2)": {
+        marginTop: "-80px"
+    }
+  },
+  card: {
+    position: "absolute",
+    "&:nth-child(1)": {
+      top: 30,
+      left: 80,
+      transform: "rotate(-18deg)"
+    },
+    "&:nth-child(2)": {
+      top: 15,
+      left: 150,
+      transform: "rotate(-9deg)"
+    },
+    "&:nth-child(3)": {
+      top: 15,
+      left: 220,
+      transform: "rotate(9deg)"
+    },
+    "&:nth-child(4)": {
+      top: 30,
+      left: 290,
+      transform: "rotate(18deg)"
+    }
   }
 }));
 
 function Hand(props) {
   const { cards } = props;
   const classes = useStyles();
-  const transforms = [
-    "rotate(-18deg)",
-    "rotate(-9deg)",
-    "rotate(9deg)",
-    "rotate(18deg)"
-  ];
 
   return (
     <div className={classes.root}>
       <div className={classes.row}>
         {cards.slice(0, 4).map((card, index) => (
-            <div style={{ transform: transforms[index]}}>
-                <Card key={`${card.typ}${index}`} color={card.typ} value={card.val} />
-            </div>
+          <div className={classes.card}>
+            <Card
+              key={`${card.typ}${index}`}
+              color={card.typ}
+              value={card.val}
+            />
+          </div>
         ))}
       </div>
       <div className={classes.row}>
         {cards.slice(4).map((card, index) => (
-          <Card key={`${card.typ}${index}`} color={card.typ} value={card.val} />
+          <div className={classes.card}>
+            <Card
+              key={`${card.typ}${index}`}
+              color={card.typ}
+              value={card.val}
+            />
+          </div>
         ))}
       </div>
     </div>
