45574cd7ba9cb082b5e6356f7e0afa5378c8a520
Nick James <thenickjames@gmail.com>
2020-01-03 10:22:13 -0800

    fix typo in process

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -1,10 +1,9 @@
-export const API_URL = proces.env.NODE_ENV === 'dev' ?
+export const API_URL = process.env.NODE_ENV === 'dev' ?
   'http://api.lostcities.local' :
   'https://api.lostcities.nickjames.io';
-export const SOCKET_URL = proces.env.NODE_ENV === 'dev' ?
+export const SOCKET_URL = process.env.NODE_ENV === 'dev' ?
   'http://api.lostcities.local' :
   'https://api.lostcities.nickjames.io';
-
 
 export const ItemTypes = {
   FACE_UP: "face_up_card",
